63d1a82695ddff8f9c521120cedfded8a8744a78
fermingimpel <fermingimpel@gmail.com>
2022-02-12 16:24:13 -0300

    Añadido movimiento basic del player

---
+++ Assets/Scripts/Player/PlayerCameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraMovement : MonoBehaviour {
+ 
+    [SerializeField] float mouseSensitivity;
+
+    [SerializeField] Transform playerBody;
+
+    float xRotation = 0;
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() {
+       // if (GameStateManager.instance.GetState() == GameStateManager.GameState.Paused)
+       //     return;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float speed;
+
+    [SerializeField] float gravity = -9.81f;
+
+    [SerializeField] float jumpHeight;
+
+    [SerializeField] Transform groundCheck;
+
+    [SerializeField] float groundDistance = 0.4f;
+
+    [SerializeField] LayerMask groundMask;
+
+    Vector3 velocity;
+    Vector3 movement;
+
+    enum PlayerState { InGround, InAir }
+    [SerializeField] PlayerState playerState;
+
+    void Start() {
+        playerState = PlayerState.InGround;
+    }
+    void Update() {
+        // if (GameStateManager.instance.GetState() == GameStateManager.GameState.Paused)
+        //     return;
+
+
+
+        if (Physics.CheckSphere(groundCheck.position, groundDistance, groundMask))
+            playerState = PlayerState.InGround;
+        else
+            playerState = PlayerState.InAir;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        movement = transform.right * x + transform.forward * z;
+
+        switch (playerState) {
+            case PlayerState.InGround:
+                if (velocity.y < 0)
+                    velocity.y = -2f;
+
+                if (Input.GetButtonDown("Jump"))
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+                break;
+            case PlayerState.InAir:
+                //
+                break;
+        }
+
+        characterController.Move(movement * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    public void SlowPlayer() {
+        StartCoroutine(Slow(50, 1));
+    }
+
+    public IEnumerator Slow(float percent, float time) {
+        float aux = speed;
+
+        speed -= (speed / 100f) * percent;
+        yield return new WaitForSeconds(time);
+
+        while (playerState == PlayerState.InAir)
+            yield return null;
+
+        speed = aux;
+    }
+}

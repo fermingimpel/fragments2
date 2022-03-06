e3b06afdc8933a5bb1c6e96a4e44487cb6a62177
fermingimpel <fermingimpel@gmail.com>
2022-03-05 21:05:40 -0300

    Added basic puzzle for bells
    When you shoot the right bell in the right order multiple time, you complete puzzle, and do thins what you gonna do, never gonna give you up

---
+++ Assets/Scripts/Puzzles/Bell Puzzle/Bell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Bell : MonoBehaviour, PuzzleInteractable {
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip bellSound;
+
+    [SerializeField] List<int> bellHitsID;
+
+    public static Action<Bell> ShootedBell;
+
+
+    void ShootBell() {
+        ShootedBell?.Invoke(this);
+        audioSource.PlayOneShot(bellSound);
+    }
+
+    public int GetBellHitId(int index) {
+        if (index >= bellHitsID.Count || bellHitsID[index] == 0)
+            return -1;
+
+        return bellHitsID[index];
+    }
+
+    public void Interact() {
+        ShootBell();
+    }
+}
---
+++ Assets/Scripts/Puzzles/Bell Puzzle/BellPuzzle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellPuzzle : MonoBehaviour {
+
+    [SerializeField] List<Bell> bells;
+    [SerializeField] int maxRoundsOfSounds;
+    int actualRoundOfBells = 0;
+    int actualBellIDNeededToContinueThePuzzleXD = 1;
+    bool puzzleCompleted = false;
+
+    void Awake() {
+        Bell.ShootedBell += CheckBellID;
+    }
+
+    void OnDisable() {
+        Bell.ShootedBell -= CheckBellID;
+    }
+
+    void OnDestroy() {
+        Bell.ShootedBell -= CheckBellID;
+    }
+
+    void CheckBellID(Bell bell) {
+        if (puzzleCompleted)
+            return;
+
+        if(bell.GetBellHitId(actualRoundOfBells) != actualBellIDNeededToContinueThePuzzleXD) {
+            actualRoundOfBells = 0;
+            actualBellIDNeededToContinueThePuzzleXD = 1;
+            Debug.Log("LE PEGASTE A LA CAMPANITA QUE NO ERA PAAA");
+            return;
+        }
+
+        Debug.Log("LE PEGASTE A LA CAMPANITA QUE ERA PAAA");
+
+        actualBellIDNeededToContinueThePuzzleXD++;
+        if(actualBellIDNeededToContinueThePuzzleXD > bells.Count) {
+            actualBellIDNeededToContinueThePuzzleXD = 1;
+            actualRoundOfBells++;
+            if(actualRoundOfBells >= maxRoundsOfSounds) {
+                PuzzleCompleted();
+            }
+            Debug.Log("TERMINASTE UNA RONDA PAA");
+        }
+    }
+
+    void PuzzleCompleted() {
+        puzzleCompleted = true;
+    }
+
+}

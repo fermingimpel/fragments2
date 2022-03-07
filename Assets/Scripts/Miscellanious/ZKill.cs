121c2a8c363eea1c6b2f90a0420292babbb0ebf7
fermingimpel <fermingimpel@gmail.com>
2022-03-07 20:23:58 -0300

    Agregade zkill

---
+++ Assets/Scripts/Miscellanious/ZKill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZKill : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+            other.GetComponent<PlayerController>().Hit(9999);
+        else if (other.CompareTag("Enemy"))
+            other.GetComponent<Enemy>().Hit(99999, Vector3.zero, Vector3.zero);
+    }
+}

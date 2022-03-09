d83c7e7c19ee6893c6b2a9d00f141ef7f1b8cbb3
fermingimpel <fermingimpel@gmail.com>
2022-03-09 13:06:49 -0300

    Ahora el outline de los objetos solo se dibuja si estas cerca de ellos

---
+++ Assets/Scripts/Utils/OutlineDistanceChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class OutlineDistanceChecker : MonoBehaviour {
+
+    [SerializeField] Outline outline;
+    [SerializeField] Transform player;
+    [SerializeField] float distanceToDrawOutline = 6;
+
+    void Start() {
+        outline = GetComponent<Outline>();
+        player = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void FixedUpdate() {
+        if(Vector3.Distance(transform.position, player.position) < distanceToDrawOutline) 
+            outline.eraseRenderer = false;
+        else
+            outline.eraseRenderer = true;
+    }
+}
